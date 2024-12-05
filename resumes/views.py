ba785977fdfd1a78410fadb6039d52562a8d8c2e
JW.921 <a27371510@gmail.com>
2024-12-05 20:27:16 +0800

    update

--- resumes/views.py
+++ resumes/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Resume
 from django.contrib import messages
+from .forms import ResumeForm
+
 
 def index(request):
     if request.POST:
-        resume = Resume()
-        resume.title = request.POST.get("title")
-        resume.skill = request.POST.get("skill")
-        resume.content = request.POST.get("content")
-        resume.save()
+        form = ResumeForm(request.POST)
+        form.save()
+        # resume.title = request.POST.get("title")
+        # resume.skill = request.POST.get("skill")
+        # resume.location = request.POST.get("location")
+        # resume.content = request.POST.get("content")
+        # resume.save()
         
         messages.success(request, "新增成功")
-
         return redirect("resumes:index")
     
     resumes = Resume.objects.all()
@@ -23,15 +26,23 @@ def index(request):
 
 
 def new(request):
-    return render(request, "resumes/new.html")
+    form = ResumeForm()
+    return render(
+        request, 
+        "resumes/new.html",
+        {"form": form}
+    )
 
 def show(request, id):
     resume = get_object_or_404(Resume, id=id)
     if request.POST:
-        resume.title = request.POST.get("title")
-        resume.skill = request.POST.get("skill")
-        resume.content = request.POST.get("content")
-        resume.save()
+        form = ResumeForm(request.POST, instance=resume)
+        form.save()
+        # resume.title = request.POST.get("title")
+        # resume.skill = request.POST.get("skill")
+        # resume.location = request.POST.get("location")
+        # resume.content = request.POST.get("content")
+        # resume.save()
 
         messages.success(request, "更新成功")
         return redirect("resumes:index")
@@ -43,16 +54,18 @@ def show(request, id):
     )
 def edit(request, id):
     resume = get_object_or_404(Resume, id=id)
+    form = ResumeForm(instance=resume)
 
     return render(
         request, 
         "resumes/edit.html", 
-        {"resume": resume},
+        {"resume": resume,
+         "form": form,
+        },
     )
 
 def delete(request, id):
     resume = get_object_or_404(Resume, id=id)
-
     if request.POST:
         resume.delete()
         messages.success(request, "刪除成功")
